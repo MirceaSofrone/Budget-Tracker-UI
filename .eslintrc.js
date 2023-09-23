7e145a0ce15ec7da3a059244b0313f57108fb0b0
Mircea Sofrone <mirceasofrone@Mirceas-Mac-mini.local>
2023-09-23 15:44:00 +0300

    Linter added

---
+++ .eslintrc.js
@@ -0,0 +1,58 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:eslint-plugin-sonarjs/recommended',
+  ],
+  'overrides': [
+    {
+      'env': {
+        'node': true
+      },
+      'files': [
+        '.eslintrc.{js,cjs}'
+      ],
+      'parserOptions': {
+        'sourceType': 'script'
+      }
+    }
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    '@typescript-eslint'
+  ],
+  'ignorePatterns': [
+    'node_modules',
+    'dist',
+    'coverage',
+    'src/**/*.spec.ts',
+    '*.scss',
+    'index.html',
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
+};
---
+++ .lintstagedrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  '**/*.{ts,html}': ['npm run lint', 'git add'],
+};
